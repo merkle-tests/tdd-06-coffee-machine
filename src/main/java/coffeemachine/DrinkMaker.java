dddd961278bec9357b61a68f2d0e75165681fca8
enr02 <erodriguez@merkleinc.com>
2019-05-31 14:21:14 +0200

    Initial commit

---
+++ src/main/java/coffeemachine/DrinkMaker.java
@@ -0,0 +1,8 @@
+package coffeemachine;
+
+public class DrinkMaker {
+
+    public void makeDrink(String command){
+        throw new RuntimeException("this shouldn't be called in test code");
+    }
+}
---
+++ src/test/java/coffeemachine/CoffeeMachineTest.java
@@ -0,0 +1,12 @@
+package coffeemachine;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CoffeeMachineTest {
+
+    @Test
+    public void fooTest() {
+        Assert.assertTrue(false);
+    }
+}
